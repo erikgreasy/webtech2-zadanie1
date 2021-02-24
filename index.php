9c46209799a10ce2b30d4c5d8d7aaf1fa2527016
erik.masny <erik.masny@gmail.com>
2021-02-24 11:06:33 +0100

    Init

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1 | Erik Masny</title>
+</head>
+<body>
+  
+
+    <main>
+
+        <?php
+
+            $files = scandir('files');
+            
+
+        ?>
+
+        <table>
+            <tr>
+                <th>Meno</th>
+                <th>Veľkosť</th>
+                <th>Dátum</th>
+            </tr>
+
+            <?php foreach( $files as $file ): ?>
+                <?php if( $file != '.' && $file != '..' ): ?>
+                    <tr>
+                        <td><?= $file ?></td>
+                        <td><?= filesize( 'files/'.$file ) ?></td>
+                        <td><?= date( 'd.m.Y',  filectime( 'files/'.$file ) ) ?></td>
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </table>
+
+    </main>
+
+</body>
+</html>
