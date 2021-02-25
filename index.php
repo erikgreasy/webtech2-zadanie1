8b6546664cc39a6773ce6ac2f42438fb6b3cce94
erik.masny <erik.masny@gmail.com>
2021-02-25 08:26:10 +0100

    Added jquery and DataTables

--- index.php
+++ index.php
@@ -5,39 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+
     <title>Zadanie 1 | Erik Masny</title>
 </head>
 <body>
   
 
     <main>
+        <div class="container">
+            <?php
 
-        <?php
+                $files = scandir('files');
+                
 
-            $files = scandir('files');
-            
+            ?>
 
-        ?>
-
-        <table>
-            <tr>
-                <th>Meno</th>
-                <th>Veľkosť</th>
-                <th>Dátum</th>
-            </tr>
-
-            <?php foreach( $files as $file ): ?>
-                <?php if( $file != '.' && $file != '..' ): ?>
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?= $file ?></td>
-                        <td><?= filesize( 'files/'.$file ) ?></td>
-                        <td><?= date( 'd.m.Y',  filectime( 'files/'.$file ) ) ?></td>
+                        <th id="name-th">Meno</th>
+                        <th id="size-th">Veľkosť</th>
+                        <th id="date-th">Dátum</th>
                     </tr>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </table>
+                </thead>
+
+                <tbody>
+
+                    <?php foreach( $files as $file ): ?>
+                        <?php if( $file != '.' && $file != '..' ): ?>
+                            <tr class="table-row">
+                                <td class="name"><?= $file ?></td>
+                                <td class="size"><?= filesize( 'files/'.$file ) ?></td>
+                                <td><?= date( 'd.m.Y',  filectime( 'files/'.$file ) ) ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
 
     </main>
 
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="js/app.js"></script>
 </body>
 </html>
---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function($) {
+
+    $('.table').DataTable();
+    // var rows = $('.table-row');
+    // console.log(rows)
+
+    // rows.DataTable()
+    // $( '#name-th' ).click( function() {
+    //     rows.sort(function(a, b) {
+    //         return $(a).find('.name') < $(b).find('.name');
+    //     })
+    //     rows.each( function(index, row) {
+    //         console.log(row)
+    //     } )
+    // } )
+
+    // $( '#size-th' ).click( function() {
+    //     rows.sort(function(a, b) {
+    //         return $(a).find('.size').html() < $(b).find('.size').html();
+    //     })
+    //     rows.each( function(index, row) {
+    //         console.log( $(row).find('.size').html() )
+    //     } )
+    // } )
+
+});
