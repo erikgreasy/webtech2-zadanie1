b3add24ec5498e46a0e67ec5c5fb61d74087254a
erik.masny <erik.masny@gmail.com>
2021-02-26 12:51:29 +0100

    Changed files loop and removed all JS (not needed)

--- index.php
+++ index.php
@@ -1,14 +1,65 @@
 <?php
 
     if( isset( $_GET[ 'folder' ] ) && $_GET['folder'] != 'files' ) {
-        $folder = $_GET[ 'folder' ] . '/';
+        $folder = rtrim( $_GET[ 'folder' ], '/' ) . '/';
     } else {
         $folder = 'files/';
     }
+
     $files = scandir( $folder );
 
+    $files_arr = [];
+    foreach( $files as $file ) {
+        if( $file == '.' ) {
+            continue;
+        }
+
+        
+        $single_file = [];
+        $single_file['name'] = $file;
+        $single_file['size'] = filesize( $folder.$file );
+        $single_file['date'] = filectime( $folder.$file );
+        
+        
+        if( $file == '..' ) {
+            $single_file['size'] = 0;
+            $single_file['date'] = 0;
+        }
+        
+        $files_arr[] = $single_file;
+    }
+
+
+    if( isset( $_GET[ 'order' ] ) ) {
+        $order = $_GET[ 'order' ];
+        switch($order) {
+            case 'name':
+                sort( $files );
+                break;
+            case 'size':
+                usort( $files_arr, function($a, $b) {
+                    if( $a['size'] == $b['size'] ) {
+                        return 0;
+                    }
+            
+                    return ($a['size'] < $b['size']) ? -1 : 1;
+                } );
+                break;
+            case 'date':
+                usort( $files_arr, function($a, $b) {
+                    if( $a['date'] == $b['date'] ) {
+                        return 0;
+                    }
+            
+                    return ($a['date'] < $b['date']) ? -1 : 1;
+                } );
+                break;
+                break;
+        } 
+    }
+    
     // echo '<pre>';
-    // var_dump( $files );
+    // var_dump( $files_arr );
     // echo '</pre>';
 ?>
 
@@ -28,54 +79,70 @@
 <body>
   
 
-    <main>
+    <main class="py-5">
         <div class="container">
             <table class="table">
                 <thead>
                     <tr>
-                        <th id="name-th">Meno</th>
-                        <th id="size-th">Veľkosť</th>
-                        <th id="date-th">Dátum</th>
+                        <th id="name-th">
+                            <form action="" method="GET">
+                                <input type="hidden" name="folder" value="<?= $folder ?>">
+                                <input type="hidden" name="order" value="name">
+                                <input type="submit" value="Meno">
+                            </form>
+                        </th>
+                        <th id="size-th">
+                            <form action="" method="GET">
+                                <input type="hidden" name="folder" value="<?= $folder ?>">
+                                <input type="hidden" name="order" value="size">
+                                <input type="submit" value="Veľkosť">
+                            </form>
+                        </th>
+                        <th id="date-th">
+                            <form action="" method="GET">
+                                <input type="hidden" name="folder" value="<?= $folder ?>">
+                                <input type="hidden" name="order" value="date">
+                                <input type="submit" value="Dátum">
+                            </form>
+                        </th>
                     </tr>
                 </thead>
 
                 <tbody>
 
-                    <?php foreach( $files as $file ): ?>
-                        <?php if( $file != '.' ): ?> 
-                            <?php if( $file == '..' ): ?>
-                                <?php if( $folder != 'files/' ): ?>
-                                    <tr class="table-row">
-                                        <td class="name">
-                                            <form action="" method="GET">
-                                                <input type="hidden" name="folder" id=".." value="<?= dirname( $folder, 1 ) ?>">
-                                                <input type="submit" value="<?= $file ?>">
-                                            </form>
-                                        </td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                <?php endif; ?>
-                            <?php else: ?>
+                    <?php foreach( $files_arr as $file ): ?>
+                        <?php if( $file['name'] == '..' ): ?>
+                            <?php if( $folder != 'files/' ): ?>
                                 <tr class="table-row">
-                                    <?php if( is_dir( $folder . $file ) ): ?>
-                                        <td class="name">
-                                            <form action="" method="GET">
-                                                <input type="hidden" name="folder" id="<?= $file ?>" value="<?= $folder . $file ?>">
-                                                <input type="submit" value="<?= $file ?>">
-                                            </form>
-                                        </td>
-                                        <td></td>
-                                        <td></td>
-                                    <?php else: ?>
-                                        <td class="name">
-                                            <?= $file ?>
-                                        </td>
-                                        <td class="size"><?= filesize( $folder.$file ) ?></td>
-                                        <td><?= date( 'd.m.Y',  filectime( $folder.$file ) ) ?></td>
-                                    <?php endif; ?>
+                                    <td class="name">
+                                        <form action="" method="GET">
+                                            <input type="hidden" name="folder" id=".." value="<?= dirname( $folder, 1 ) ?>">
+                                            <input type="submit" value="<?= $file['name'] ?>">
+                                        </form>
+                                    </td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                             <?php endif; ?>
+                        <?php else: ?>
+                            <tr class="table-row">
+                                <?php if( is_dir( $folder . $file['name'] ) ): ?>
+                                    <td class="name">
+                                        <form action="" method="GET">
+                                            <input type="hidden" name="folder" id="<?= $file ?>" value="<?= $folder . $file['name'] ?>">
+                                            <input type="submit" value="<?= $file['name'] ?>">
+                                        </form>
+                                    </td>
+                                    <td></td>
+                                    <td></td>
+                                <?php else: ?>
+                                    <td class="name">
+                                        <?= $file['name'] ?>
+                                    </td>
+                                    <td class="size"><?= $file['size'] ?></td>
+                                    <td><?= date( 'd.m.Y H:m:s',  $file['date'] ) ?></td>
+                                <?php endif; ?>
+                            </tr>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </tbody>
