15e4bceba5d38e56175387c922aa8a97d2f30dda
erik.masny <erik.masny@gmail.com>
2021-02-28 13:42:43 +0100

    Removed unnecessary css and script from header and footer

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
 
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="js/app.js"></script>
-
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="css/app.css">
 
+    <!-- STYLES -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/app.css">
 
     <title>Zadanie 1 | Erik Masny</title>
 </head>
