f20cf8e8dc36567af185e31ae305c438dba9a35c
erik.masny <erik.masny@gmail.com>
2021-02-28 13:41:20 +0100

    Removed data tables lib from footer

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
 <script src="js/app.js"></script>
 
 </body>
