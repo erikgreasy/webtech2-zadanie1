56a1de1f802a538b209a02af10610f77a8205bd8
erik.masny <erik.masny@gmail.com>
2021-02-28 12:51:30 +0100

    Added separate header and footer php files

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -43,20 +43,7 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="sk">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="css/app.css">
-
-
-    <title>Zadanie 1 | Erik Masny</title>
-</head>
-<body>
+<?php require_once 'header.php' ?>
   
 
     <main id="upload" class="py-5">
@@ -97,8 +84,4 @@
 
     </main>
 
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-<script src="js/app.js"></script>
-</body>
-</html>
+<?php require_once 'footer.php' ?>
